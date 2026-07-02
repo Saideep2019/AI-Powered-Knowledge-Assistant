79f66000ec761e275ecf600c50ebe7abd833b71a
Saideep2019 <saideepambari@gmail.com>
2026-07-01 20:37:58 -0700

    Allow Vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -35,7 +35,9 @@ client = Groq(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000",
-                   "http://127.0.0.1:3000",],
+                   "http://127.0.0.1:3000",
+                   "https://documind-frontend-nine.vercel.app"
+                   ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
